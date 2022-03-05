e473230a4cff36d72f0a4ae3a0c61adbf463f17e
Eric Moore <emoore.tech@gmail.com>
2022-03-05 14:02:02 -0600

    FSDI_114 class2

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#define function sort characters <- parameters: list of characters;
+    # swap_detected = True
+    #loop until swap detected is False
+    # for each index in the range of len(char_list_a) and store index temporarily in "idx"
+        # if char in idx > character at idx +1:
+            # #temp_var = char_list_a[idx] 
+            # temp_var_2 = char_list_a[idx+1]
+    #else:
+        #swap_detected is False
+# return list_of_characters
+
 # define function "is_anagram" <- parameters: string_a, string_b
 #if length of string_a != length of string_b; then
 #    return False
@@ -9,7 +20,15 @@
 #loop through every character in string_a and store value temporarily in "char_a":
 #      char_list.append(char_a) append char_a to char_list
 #loop through every character in string_b and store value temporarily in "char_b":
-#       append char_b to char_list_b
+#       append char_b to char_list_b]
+# loop through every character in char_list_a: and store value temporarily in char_a: 
+
+#char_list_a = sort_characters(char_list_a)
+#char_list_b = sort_characters(char_list_b)
+
+#return true if char_list_a  = char_list_b
+    
+
 
 
 NO_OF_CHARS = 256
@@ -17,6 +36,10 @@ NO_OF_CHARS = 256
 #user input
 string_a = input("Enter string_a: ")
 string_b = input("Enter string_b: ")
+
+string_a = string_a.upper()
+string_b = string_b.upper()
+
 #check length of words
 def is_anagram(string_a, string_b):
 
